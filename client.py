b62b121ec7193a60e25cbda9151b67be062f87a2
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2022-05-02 14:45:19 -0600

    Modifed code for Echo server-client.

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('localhost', 10000)
+print('connecting to %s port %s' % (server_address), file = sys.stderr)
+sock.connect(server_address)
+
+try:
+    message = 'This is the message. It will be repeated.'
+    print('sending "%s"' % (message), file = sys.stderr)
+    sock.sendall(message)
+
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print('received "%s"' % (data), file = sys.stderr)
+
+finally:
+    print('closing socket', file = sys.stderr)
+    sock.close()
---
+++ server.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('localhost', 10000)
+# updated format for print to stderr
+print('starting up on %s port %s' % (server_address), file = sys.stderr)
+sock.bind(server_address)
+
+sock.listen(1)
+
+while True:
+    print('waiting for a connection', file = sys.stderr)
+    connection, client_address = sock.accept()
+
+try:
+    print('connection from', client_address, file = sys.stderr)
+
+    while True:
+        data = connection.recv(16)
+        print('received "%s"' % (data), file = sys.stderr)
+        if data:
+            print('sending data back to the client', file = sys.stderr)
+            connection.sendall(data)
+        else:
+            print('no more data from', client_address)
+            break
+finally:
+    connection.close()
